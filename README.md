50e031e20950883d2b7d335bb115dbcc4f1c2f3a
John Evans <lgastako@gmail.com>
2020-06-01 22:41:37 -0400

    Initial implementation.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # binsert
+
+Insert a line in a file via binary insert.
+
+Prompted by:
+https://old.reddit.com/r/programmingrequests/comments/gui4g8/looking_for_command_line_utility_that_lets_user/
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,73 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
-import Lib
+import Control.Monad      ( unless
+                          , when
+                          )
+import Data.Char          ( toUpper )
+import Data.List          ( splitAt )
+import System.Directory   ( doesDirectoryExist
+                          , doesFileExist
+                          )
+import System.Environment ( getArgs )
+import System.Exit        ( die )
+import System.IO          ( BufferMode( NoBuffering )
+                          , hSetBuffering
+                          , stdout
+                          )
+import Text.Read          ( readMaybe )
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= \case
+  []     -> oneFilePlease
+  [path] -> insertIntoFile path
+  more   -> oneFilePlease
+  where
+    oneFilePlease = die "Please enter exactly one file on the command line."
+
+insertIntoFile :: FilePath -> IO ()
+insertIntoFile path = do
+  hSetBuffering stdout NoBuffering
+  ensureExists path
+  entries <- lines <$> readFile path
+  putStr "Enter new item: "
+  item <- getLine
+  entries' <- insertIntoItems item entries
+  writeFile path (unlines entries')
+
+ensureExists :: FilePath -> IO ()
+ensureExists path = do
+  d <- doesDirectoryExist path
+  when d $ die $ "Path " ++ show path ++ " exists but is a directory, not a file!"
+  f <- doesFileExist path
+  unless f $ writeFile path ""
+
+data Choice = Q | B | A
+  deriving (Eq, Read, Show)
+
+insertIntoItems :: String -> [String] -> IO [String]
+insertIntoItems item []    = pure [item]
+insertIntoItems item items = go ([], items, [])
+  where
+    go (before, []    , after) = pure $ before ++ item:after
+    go (before, during, after) = do
+      let idx        = length during `div` 2
+          (pre, suf) = splitAt idx during
+          (pre', suf', target) = case (pre, suf) of
+            ([], [])   -> error "how do this occur?"
+            (ps, s:ss) -> (ps, ss, s)
+            (ps, [])   -> (init ps, [], last ps)
+      putStrLn $ "Select whether the item should go [A]bove or [B]elow the target"
+      putStrLn $ "(Or [Q]uit to abort)."
+      putStrLn $ "    item: " ++ show item
+      putStrLn $ "  target: " ++ show target
+      getChoice >>= \case
+        A -> (before ++) . (++ (target:suf' ++ after)) <$> insertIntoItems item pre'
+        B -> ((before ++ pre' ++ [target]) ++) . (++ after) <$> insertIntoItems item suf'
+        Q -> die "Aborted."
+
+    getChoice = do
+      putStr $ "> "
+      s <- map toUpper <$> getLine
+      maybe (putStrLn "Pease enter 'a', 'b' or 'q'." >> getChoice) pure $ readMaybe s
